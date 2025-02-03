de8ef9a944f534595853beadae08646506d7d09f
Jonas Bauer <jonas.bauer90@outlook.de>
2025-02-03 17:59:04 +0100

    added: emojis to console.logs

--- automation/scrape.js
+++ automation/scrape.js
@@ -2,15 +2,15 @@ const { chromium } = require('playwright');
 
 // Browser und Page Setup und trigger des Tests
 (async () => {
-    console.log("------- STEP-1 Torhüter -------");
+    console.log("🧤------- STEP-1 Torhüter -------🧤");
     await downloadTor();
-    console.log("------- STEP-2 Abwehr ---------");
+    console.log("🧱------- STEP-2 Abwehr ---------🧱");
     await downloadAbw();
-    console.log("------- STEP-3 Mittelfeld -----");
+    console.log("⚽------- STEP-3 Mittelfeld -----⚽");
     await downloadMf();
-    console.log("------- STEP-4 Sturm ----------");
+    console.log("🥅------- STEP-4 Sturm ----------🥅");
     await downloadSt();
-    console.log("------- FINISHED --------------");
+    console.log("🏆------- FINISHED --------------🏆");
 })();
 
 
@@ -27,9 +27,9 @@ async function downloadTor() {
         await page.waitForSelector('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_3"]/div[2]/input', { visible: true });
         // Klicke die Checkbox für "Torhüter"
         await page.click('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_3"]/div[2]/input', { timeout: 5000 });
-        console.log('Checkbox "Torhüter" erfolgreich geklickt.');
+        console.log('✔️ Checkbox "Torhüter" erfolgreich geklickt.');
     } catch (error) {
-        console.error('Fehler beim Klicken auf "Torhüter" Checkbox:');
+        console.error('❌ Fehler beim Klicken auf "Torhüter" Checkbox:');
     }
     
     // Falls notwendig, warte erneut auf eine Aktion nach dem Klick
@@ -38,9 +38,9 @@ async function downloadTor() {
     try {
         // Warten bis die "(All)" Checkbox sichtbar ist
         await page.keyboard.press('Escape', { timeout: 5000 });
-        console.log('ESC gedrückt');
+        console.log('✔️ ESC gedrückt');
     } catch (error) {
-        console.log('Fehler beim ESC drücken!');
+        console.log('❌ Fehler beim ESC drücken!');
     }
     await page.waitForTimeout(1000);
 
@@ -65,9 +65,9 @@ async function downloadAbw() {
         await page.waitForSelector('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_0"]/div[2]/input', { visible: true });
         // Klicke die Checkbox für "Abwehr"
         await page.click('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_0"]/div[2]/input', { timeout: 5000 });
-        console.log('Checkbox "Abwehr" erfolgreich geklickt.');
+        console.log('✔️ Checkbox "Abwehr" erfolgreich geklickt.');
     } catch (error) {
-        console.error('Fehler beim Klicken auf "Abwehr" Checkbox:');
+        console.error('❌ Fehler beim Klicken auf "Abwehr" Checkbox:');
     }
     
     // Falls notwendig, warte erneut auf eine Aktion nach dem Klick
@@ -76,9 +76,9 @@ async function downloadAbw() {
     try {
         // Warten bis die "(All)" Checkbox sichtbar ist
         await page.keyboard.press('Escape', { timeout: 5000 });
-        console.log('ESC gedrückt');
+        console.log('✔️ ESC gedrückt');
     } catch (error) {
-        console.log('Fehler beim ESC drücken!');
+        console.log('❌ Fehler beim ESC drücken!');
     }
     await page.waitForTimeout(1000);
 
@@ -103,9 +103,9 @@ async function downloadMf() {
         await page.waitForSelector('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_1"]/div[2]/input', { visible: true });
         // Klicke die Checkbox für "Mittelfeld"
         await page.click('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_1"]/div[2]/input', { timeout: 5000 });
-        console.log('Checkbox "Mittelfeld" erfolgreich geklickt.');
+        console.log('✔️ Checkbox "Mittelfeld" erfolgreich geklickt.');
     } catch (error) {
-        console.error('Fehler beim Klicken auf "Mittelfeld" Checkbox:');
+        console.error('❌ Fehler beim Klicken auf "Mittelfeld" Checkbox:');
     }
     
     // Falls notwendig, warte erneut auf eine Aktion nach dem Klick
@@ -114,9 +114,9 @@ async function downloadMf() {
     try {
         // Warten bis die "(All)" Checkbox sichtbar ist
         await page.keyboard.press('Escape', { timeout: 5000 });
-        console.log('ESC gedrückt');
+        console.log('✔️ ESC gedrückt');
     } catch (error) {
-        console.log('Fehler beim ESC drücken!');
+        console.log('❌ Fehler beim ESC drücken!');
     }
     await page.waitForTimeout(1000);
 
@@ -141,9 +141,9 @@ async function downloadSt() {
         await page.waitForSelector('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_2"]/div[2]/input', { visible: true });
         // Klicke die Checkbox für "Sturm"
         await page.click('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_2"]/div[2]/input', { timeout: 5000 });
-        console.log('Checkbox "Sturm" erfolgreich geklickt.');
+        console.log('✔️ Checkbox "Sturm" erfolgreich geklickt.');
     } catch (error) {
-        console.error('Fehler beim Klicken auf "Sturm" Checkbox:');
+        console.error('❌ Fehler beim Klicken auf "Sturm" Checkbox:');
     }
     
     // Falls notwendig, warte erneut auf eine Aktion nach dem Klick
@@ -152,9 +152,9 @@ async function downloadSt() {
     try {
         // Warten bis die "(All)" Checkbox sichtbar ist
         await page.keyboard.press('Escape', { timeout: 5000 });
-        console.log('ESC gedrückt');
+        console.log('✔️ ESC gedrückt');
     } catch (error) {
-        console.log('Fehler beim ESC drücken!');
+        console.log('❌ Fehler beim ESC drücken!');
     }
     await page.waitForTimeout(1000);
 
@@ -174,9 +174,9 @@ async function removePosFilter (page) {
         await page.wait
         await page.waitForSelector('//*[@id="tableau_base_widget_LegacyCategoricalQuickFilter_1"]/div/div[3]/span/div[1]', { visible: true });
         await page.click('//*[@id="tableau_base_widget_LegacyCategoricalQuickFilter_1"]/div/div[3]/span/div[1]', { timeout: 5000 });
-        console.log('Position erfolgreich geklickt.');
+        console.log('✔️ Position erfolgreich geklickt.');
     } catch (error) {
-        console.error('Fehler beim Klicken auf Position');
+        console.log('❌ Fehler beim Klicken auf Position');
     }
 
     try {
@@ -184,9 +184,9 @@ async function removePosFilter (page) {
         await page.wait
         await page.waitForSelector('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_(Alle)"]/div[2]/input', { visible: true });
         await page.click('//*[@id="FI_federated.1h995zk13cukhd13qkcd91wnq4wd,none:Position:nk15566219379631039534_3142159161780403884_(Alle)"]/div[2]/input', { timeout: 5000 });
-        console.log('Checkbox "(All)" erfolgreich geklickt.');
+        console.log('✔️ Checkbox "(All)" erfolgreich geklickt.');
     } catch (error) {
-        console.error('Fehler beim Klicken auf "(All)" Checkbox');
+        console.log('❌ Fehler beim Klicken auf "(All)" Checkbox');
     }
     
     // Warte kurz, damit das UI Zeit zum Aktualisieren hat
@@ -197,17 +197,22 @@ async function removePosFilter (page) {
 async function openPage (page) {
      // Tableau-URL anpassen
      const tableauUrl = 'https://public.tableau.com/views/Kickbase_Matchup_Tool_v1/Kickbase_Matchup_Tool?%3Adisplay_static_image=y&%3AbootstrapWhenNotified=true&%3Aembed=true&%3Alanguage=de-DE&:embed=y&:showVizHome=n&:apiID=host0#navType=0&navSrc=Parse';
-     console.log(`Öffne URL: ${tableauUrl}`);
      
      // Navigiere zur Tableau-Seite
-     await page.goto(tableauUrl);
+     try {
+        await page.goto(tableauUrl);
+        console.log(`✔️ Öffne URL: ${tableauUrl}`);
+     } catch (error) {
+        console.log(`❌ Fehler beim öffnen der URL: ${tableauUrl}`)
+     }
+     
  
      // Warten, bis die Seite vollständig geladen ist
      try {
          await page.waitForLoadState('load');
-         console.log('Seite vollständig geladen.');
+         console.log('✔️ Seite vollständig geladen.');
      } catch (e) {
-         console.log('Seite konnte nicht vollständig geladen werden.');
+         console.log('❌ Seite konnte nicht vollständig geladen werden.');
      }
  
      // Viewport setzen um alle 18 Teams auf das PDF zu bekommen
@@ -221,27 +226,27 @@ async function downloadPdf(page, position) {
     // Download Layout öffnen
     try {
         await page.waitForSelector('//*[@id="download"]', { timeout: 5000 });
-        console.log('Download Button');
+        console.log('✔️ Download Button gefunden');
         await page.click('//*[@id="download"]');
-        console.log('Download Layout geöffnet');
+        console.log('✔️ Download Layout geöffnet');
     } catch (e) {
-        console.log('Download Layout konnte nicht gefunden oder geöffnet werden!');
+        console.log('❌ Download Layout konnte nicht gefunden oder geöffnet werden!');
     }
 
     // 3. Klick auf PDF Button
     try {
         await page.waitForSelector('//*[@id="viz-viewer-toolbar-download-menu"]/div[2]/div/div/span[1]', { timeout: 5000 });
-        console.log('Download Layout gefunden');
+        console.log('✔️ PDF Button gefunden');
         await page.click('//*[@id="viz-viewer-toolbar-download-menu"]/div[2]/div/div/span[1]', { timeout: 7000 });
-        console.log('PDF Button geklickt');
+        console.log('✔️ PDF Button geklickt');
     } catch (e) {
-        console.log('Download Layout konnte nicht gefunden oder PDF Button nicht geklickt werden!');
+        console.log('❌ Download Layout konnte nicht gefunden oder PDF Button nicht geklickt werden!');
     }
 
     // Download-Button klicken und auf den Download warten
     try {
         await page.waitForSelector('//*[@id="export-pdf-dialog-Dialog-Body-Id"]/div/div[4]/button', { timeout: 5000 });
-        console.log('Download Button gefunden');
+        console.log('✔️ Download Button gefunden');
 
         // Auf den Download warten und dann klicken
         const [download] = await Promise.all([
@@ -252,8 +257,8 @@ async function downloadPdf(page, position) {
         // Speichern der Datei in den gewünschten Ordner
         const filePath = `automation/pdf/${position}.pdf`;
         await download.saveAs(filePath);
-        console.log(`Download erfolgreich: ${filePath}`);
+        console.log(`✅ Download erfolgreich: ${filePath}`);
     } catch (e) {
-        console.log('Download Button konnte nicht gefunden oder geklickt werden!');
+        console.log('❌ Download Button konnte nicht gefunden oder geklickt werden!');
     }
 }
